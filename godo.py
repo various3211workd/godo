a70ddc11ed59740cd69fd2f3b1a1f566a5b11e13
various3211workd <kd1229568@st.kobedenshi.ac.jp>
2019-11-02 15:50:51 +0900

    [Refactor]

--- godo.py
+++ godo.py
@@ -3,29 +3,19 @@ import sys
 from googlesearch import search
 
 def google_search(query):
+    user_agent_data = "Mozilla/4.0 (compatible; MSIE 8.0; Windows NT 6.1; Trident/4.0; SLCC2; .NET CLR 2.0.50727; .NET CLR 3.5.30729; .NET CLR 3.0.30729; Media Center PC 6.0)"
     
+    # DBGUG
     # read database query
-    #db_data = open('db/sql_injection_list.txt', 'r')
+    db_data = open('db/sql_injection_list.txt', 'r')
 
-    # debug
-    search_data = query# + ' /lire.php?rub='
-    print(search_data)
-    
-    #iterable = search(search_data, pause=4.0, num=1, user_agent="Mozilla/4.0 (compatible; MSIE 8.0; Windows NT 6.1; Trident/4.0; SLCC2; .NET CLR 2.0.50727; .NET CLR 3.5.30729; .NET CLR 3.0.30729; Media Center PC 6.0)")
-    iterable = (i for i in [])
-    
-    if next(iterable, None) is None:
-        print('Not initiated')
-        
-"""
     for data in db_data:
         search_data = query + ' ' + data
-        if not search(search_data, pause=4.0, num=1, user_agent="Mozilla/4.0 (compatible; MSIE 8.0; Windows NT 6.1; Trident/4.0; SLCC2; .NET CLR 2.0.50727; .NET CLR 3.5.30729; .NET CLR 3.0.30729; Media Center PC 6.0)"):
-            print("無いよ")
+        if next(search(search_data, pause=4.0, num=1, user_agent=user_agent_data), None) is None:
+            print('Not initiated')
         else:
             print(search_data)
-"""
-
+    
 def main():
     args = sys.argv
 
